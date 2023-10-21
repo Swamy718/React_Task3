f5ae80eb6f0c1d3f8656746e092d4407dfdea5e3
Swamy718 <144110970+Swamy718@users.noreply.github.com>
2023-10-21 12:17:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Word_count from './components/Word_count';
-// import Color_picker from './components/Color_picker';
-// import Dummy_data from './components/Dummy_data';
-import Age_calculator from './components/Age_calculator';
+import Color_picker from './components/Color_picker';
 
 function App() {
   const arr=["red","lawngreen","blue","yellow","fuchsia","skyblue","coral","darkmagenta","pink","green","orangered","deepskyblue","brown","lightcoral","darkcyan","goldenrod"]
   return (
     <div className="App">
-       {/* <Color_picker color={arr}/> */}
-       {/* <Dummy_data/> */}
-       <Age_calculator/>
+       <Color_picker color={arr}/> 
     </div>
   );
 }
